519137076163a92492930e6c353897b5c480fd19
faturrahman10 <faturrahmanhasir@gmail.com>
2025-06-28 16:59:23 +0800

    push chalenge day 1 and day 2

---
+++ day-001/chalenge.py
@@ -0,0 +1,5 @@
+print("welcome to the Band Name Generator")
+city_grew = input("what's the name of the city you grew up?\n")
+pet_name = input("what's is your pet's name?\n")
+band_name = city_grew + " " + pet_name
+print("your band name could be " + band_name)
