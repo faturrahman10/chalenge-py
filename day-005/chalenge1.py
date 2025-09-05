07db8221de7cad152ed6d1e18fa0469d612b02fc
faturrahman10 <faturrahmanhasir@gmail.com>
2025-09-05 23:42:08 +0800

    add question to chalenge 1 and 2 day 5

--- day-005/chalenge1.py
+++ day-005/chalenge1.py
@@ -1,3 +1,26 @@
+# Be a Computer
+# Predict what will be printed from the code below:
+
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " pie")
+# Indentation
+# Indentation is very important in Python programming. Every time you see the : symbol used, you need to be careful about the indentation that comes afterwards.
+
+# e.g. This code will behave very differently
+
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print("Hello")
+# from this code:
+
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+# print("Hello")
+
 fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruit)
--- day-005/chalenge2.py
+++ day-005/chalenge2.py
@@ -1,3 +1,11 @@
+# COMPLETE THIS CHALLENGE WITHOUT USING max()
+# You are given a list of exam scores, and you have to print out the highest score from the List. You will need to use what you have learnt about Lists, For Loops and Conditionals to print out the highest score in the list of student_scores. For example, if the scores were:
+
+# 8 65 89 86 55 91 64 89
+# Your code should print
+
+# 91
+
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 
 max_score = 0
